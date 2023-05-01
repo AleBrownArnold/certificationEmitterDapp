de8df3eb65c6dd66712270436c1a6ea9e0f52753
Alejandro Brown Arnold <abrown@lasegunda.com.ar>
2023-05-01 18:10:05 -0300

    Add metamask event handler method and setTokenUri method

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,7 +17,15 @@ const CONTRACT_ABI = require("../contracts/Certificate.json").abi;
 const CONTRACT_NAME = require("../contracts/Certificate.json").contractName
 
 export default class App extends React.Component {
-  state = { web3Provider: null, accounts: null, networkId: null, contract: null, storageValue: null, receiverAddress: '' };
+  state = { 
+    web3Provider: null,
+    accounts: null,
+    networkId: null,
+    contract: null,
+    storageValue: null,
+    receiverAddress: '',
+    tokenUri: null
+  };
 
   componentDidMount = async () => {
     try {
@@ -35,10 +43,17 @@ export default class App extends React.Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3Provider: web3, accounts: accounts, networkId: networkId, contract: contract });
+      this.setState({ 
+        web3Provider: web3,
+        accounts: accounts,
+        networkId: networkId,
+        contract: contract
+      });
 
       // Get the value from the contract to prove it worked and update storageValue state
       this.getMethod()
+
+      this.handleMetamaskEvent();
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -51,10 +66,10 @@ export default class App extends React.Component {
 
   //TODO: set method to interact with Storage Smart Contract
   setMethod = async () => {
-    const { accounts, contract, receiverAddress } = this.state;
+    const { accounts, contract, receiverAddress, tokenUri } = this.state;
 
     // Stores a given value, 5 by default.
-    const transaction = await contract.methods.awardItem(receiverAddress, 'testing').send({ from: accounts[0] })
+    const transaction = await contract.methods.awardItem(receiverAddress, tokenUri).send({ from: accounts[0] })
     console.log(transaction)
 
     // Get the updated value from the contract and updates storageValue state
@@ -76,12 +91,30 @@ export default class App extends React.Component {
     this.setState({ receiverAddress: address });
   }
 
+  setTokenUri = (tokenUri) => {
+    this.setState({ tokenUri: tokenUri });
+  }
+
+  handleMetamaskEvent = async () => {
+    window.ethereum.on('accountsChanged', function (accounts) {
+      // Time to reload your interface with accounts[0]!
+      alert("Incoming event from Metamask: Account changed 🦊")
+      window.location.reload()
+    })
+
+    window.ethereum.on('chainChanged', function (networkId) {
+      // Time to reload your interface with the new networkId
+      alert("Incoming event from Metamask: Network changed 🦊")
+      window.location.reload()
+    })
+  }
+
   render() {
     if (!this.state.web3Provider) {
       return <div className="App-no-web3">
         <h3>No Web3 connection... 🧐</h3>
-        <p>Jump to the next chapter to configure the Web3 Provider.</p>
-        <h3>Let's go! ⏭️</h3>
+        <p>Please connect via MetaMask for this demo.</p>
+        {/* Add auto metamask login check*/}
       </div>;
     }
     return (
@@ -91,16 +124,18 @@ export default class App extends React.Component {
         <div className="App-simplestorage-example">
 
           {/* DApp Header */}
-          <h1>Ready to go! 🚀</h1>
-          <p> Your Truffle Box has been succesfully installed ⚙️ </p>
-          <p> The default Smart Contract has been compiled and deployed on testnet 🧪 </p>
+          <h1>Certification Demo</h1>
           <br />
 
+          <h2>Metamask Connection Succesfull</h2>
+          <p>User address: {this.state.accounts}</p>
+
+
           {/* DApp Information */}
-          <h2>Smart Contract: SimpleStorage.sol 🧮</h2>
+          <h2>Smart Contract: Certificate.sol 🧮</h2>
           <p>Contract address: {CONTRACT_ADDRESS}</p>
           <p>
-            If your contracts compiled and migrated successfully, below will show
+            If your certification was sent successfully, below will show
             a stored value of 1 (by default).
           </p>
           <h3>Balance Of address: { this.state.receiverAddress }: {this.state.storageValue} </h3>
@@ -113,6 +148,14 @@ export default class App extends React.Component {
           <input 
             value={this.state.receiverAddress}
             onChange={e => this.setAddress(e.target.value)}
+            type="string"
+          />
+          <p>
+            Input token uri
+          </p>
+          <input 
+            value={this.state.tokenUri}
+            onChange={e => this.setTokenUri(e.target.value)}
             type="string"
           />
           <p>
